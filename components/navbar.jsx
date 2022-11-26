3c7d2d3eccebbe1ea21b3d258104dbc38abb5e9a
IrwanFicoFar <irwanhadi9@gmail.com>
2022-11-27 05:52:01 +0700

    button logout

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,8 +1,36 @@
+import { get } from "jquery";
 import Link from "next/link";
+import { useEffect } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import style from "../styles/nav.module.css";
 
 const MyNavbar = () => {
+  const getToken = () => {
+    const token = localStorage.getItem("jwt-token");
+    console.log(token);
+    if (token === null || undefined) {
+      return {
+        status: "INVALID",
+      };
+    }
+    return {
+      status: "VALID",
+    };
+  };
+
+  const handleLogout = () => {
+    let ans = window.confirm(`Are you sure want to logout?`);
+    if (ans) {
+      localStorage.removeItem("jwt-token");
+      alert(`Thanks for coming`);
+      window.location.href = "/login";
+    }
+  };
+
+  useEffect(() => {
+    const mytoken = getToken();
+  }, []);
+
   return (
     <Navbar className={style.navbar} variant="dark" expand="lg">
       <Container>
@@ -21,8 +49,11 @@ const MyNavbar = () => {
           </Nav>
 
           <Nav>
+            <Link href="" className={style.nav_link} onClick={handleLogout}>
+              LOGOUT
+            </Link>
             <Link href="/login" className={style.nav_link}>
-              SING UP
+              SIGN UP
             </Link>
           </Nav>
         </Navbar.Collapse>
