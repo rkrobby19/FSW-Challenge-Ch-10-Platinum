f7ecf9dc2be0486d852d1976c5607169ac210101
rkrobby19 <82077579+rkrobby19@users.noreply.github.com>
2022-11-28 23:32:23 +0700

    Feature/profile page (#10)
    * Add logic user upload image and input modal form
    
    * Add page restriction
    
    * Add user upload image to firebase storage
    
    * Add user update data to firebase
    
    * Fix redux state, minor error on update data logic to firebase
    
    * Fix typo
    
    * Fix update user data bug
    
    * Change switch button logic on navbar
    
    * Fix minor typo
    
    * Add show game history
    
    * Change already played button logic on home page

--- redux/store/index.js
+++ redux/store/index.js
@@ -2,13 +2,16 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import loadingSlice from "../reducer/loading";
 import userSlice from "../reducer/user";
 import roundsSlice from "../reducer/scores_rps";
-import scoreSlice from "../reducer/scores";
+import scoreSlice from "../reducer/score";
+
 
 const allReducers = combineReducers({
     userReducer: userSlice.reducer,
     loadingReducer: loadingSlice.reducer,
     roundsReducer: roundsSlice.reducer,
-    scoreReducer: scoreSlice.reducer
+
+    scoreReducer: scoreSlice.reducer,
+
 });
 
 const myStore = configureStore({
