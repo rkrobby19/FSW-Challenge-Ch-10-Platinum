32b4df8915c14980de61c414012647bb94539e6b
IrwanFicoFar <irwanhadi9@gmail.com>
2022-11-30 20:10:01 +0700

    finish edit

--- components/profile/edit_profile_modal.jsx
+++ components/profile/edit_profile_modal.jsx
@@ -2,115 +2,109 @@ import style from "./ProfileCard.module.css";
 import { Button, Col, Container, Form, Modal, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
-
 const EditProfileModal = (props) => {
-    const {
-        show,
-        handleClose,
-        handleOnChange,
-        handleFileChange,
-        tempImgUrl,
-        updateUserData,
-    } = props;
+  const {
+    show,
+    handleClose,
+    handleOnChange,
+    handleFileChange,
+    tempImgUrl,
+    updateUserData,
+  } = props;
 
-    const userData = useSelector((state) => {
-        return state.userReducer;
-    });
+  const userData = useSelector((state) => {
+    return state.userReducer;
+  });
 
-    return (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Profile Setting</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Container className="text-center">
-                        <img src={tempImgUrl} className={style.profilePic} />
-                    </Container>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Upload your profile pic</Form.Label>
-                        <Form.Control type="file" onChange={handleFileChange} />
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Form.Label column sm="3">
-                            Fullname
-                        </Form.Label>
-                        <Col sm="9">
-                            <Form.Control
-                                type="string"
+  return (
+    <Modal show={show} onHide={handleClose} className={style.card}>
+      <Modal.Header closeButton>
+        <Modal.Title>Profile Setting</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Container className="text-center">
+            <img src={tempImgUrl} className={style.profilePic} />
+          </Container>
+          <Form.Group className="mb-3">
+            <Form.Label>Upload your profile pic</Form.Label>
+            <Form.Control type="file" onChange={handleFileChange} />
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="3">
+              Fullname
+            </Form.Label>
+            <Col sm="9">
+              <Form.Control
+                type="string"
+                defaultValue={userData.fullname}
+                id="fullname"
+                onChange={handleOnChange}
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="3">
+              Username
+            </Form.Label>
+            <Col sm="9">
+              <Form.Control
+                type="string"
+                defaultValue={userData.username}
+                id="username"
+                onChange={handleOnChange}
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="3">
+              Email
+            </Form.Label>
+            <Col sm="9">
+              <Form.Control
+                readOnly
+                disabled
+                type="email"
+                defaultValue={userData.email}
+                id="email"
+              />
+            </Col>
+          </Form.Group>
 
-
-                                defaultValue={userData.fullname}
-
-                                id="fullname"
-                                onChange={handleOnChange}
-                            />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Form.Label column sm="3">
-                            Username
-                        </Form.Label>
-                        <Col sm="9">
-                            <Form.Control
-                                type="string"
-
-                                defaultValue={userData.username}
-
-                                id="username"
-                                onChange={handleOnChange}
-                            />
-                        </Col>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3">
-                        <Form.Label column sm="3">
-                            Email
-                        </Form.Label>
-                        <Col sm="9">
-                            <Form.Control
-                                readOnly
-                                disabled
-                                type="email"
-                                defaultValue={userData.email}
-                                id="email"
-                            />
-                        </Col>
-                    </Form.Group>
-
-                    <Form.Group as={Row} className="mb-3">
-                        <Form.Label column sm="3">
-                            Password
-                        </Form.Label>
-                        <Col sm="9">
-                            <Form.Control
-                                readOnly
-                                disabled
-                                type="password"
-                                defaultValue="*******"
-                                id="password"
-                            />
-                        </Col>
-                    </Form.Group>
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="danger" onClick={handleClose}>
-                    Discard
-                </Button>
-                <Button
-                    variant="primary"
-                    onClick={() => {
-                        handleClose();
-                        updateUserData();
-                    }}
-                >
-                    <i class="fa-solid fa-floppy-disk"></i>
-                    {"   "}
-                    Save
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label column sm="3">
+              Password
+            </Form.Label>
+            <Col sm="9">
+              <Form.Control
+                readOnly
+                disabled
+                type="password"
+                defaultValue="*******"
+                id="password"
+              />
+            </Col>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="danger" onClick={handleClose}>
+          Discard
+        </Button>
+        <Button
+          variant="primary"
+          onClick={() => {
+            handleClose();
+            updateUserData();
+          }}
+        >
+          <i class="fa-solid fa-floppy-disk"></i>
+          {"   "}
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default EditProfileModal;
--- components/profile/game_history.jsx
+++ components/profile/game_history.jsx
@@ -1,43 +1,43 @@
 import { Container, Table } from "react-bootstrap";
+import style from "../profile/ProfileCard.module.css";
 
 const GameHistory = (props) => {
-    const { userData, scoreData } = props;
-    return (
-        <Container>
-            <h2 style={{ color: "white" }}>
-                Lets Play The Game @{userData.username}
-            </h2>
+  const { userData, scoreData } = props;
+  return (
+    <Container className={style.font}>
+      <h2 style={{ color: "white" }}>
+        Lets Play The Game @<span>{userData.username} </span>
+      </h2>
 
-            <Table striped bordered hover variant="dark">
-                <thead>
-                    <tr className="text-center">
-                        <th>Game</th>
-
-                        <th>Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Rock-paper-scissor</td>
-                        <td className="text-center">
-                            {scoreData.rps ? scoreData.rps : "Not played"}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Simon</td>
-                        <td className="text-center">
-                            {scoreData.simon ? scoreData.simon : "Not Played"}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Dummy</td>
-                        <td className="text-center">
-                            {scoreData.dummy ? scoreData.dummy : "Not played"}
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
-        </Container>
-    );
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr className="text-center">
+            <th>Game</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Rock-paper-scissor</td>
+            <td className="text-center">
+              {scoreData.rps ? scoreData.rps : "Not played"}
+            </td>
+          </tr>
+          <tr>
+            <td>Simon</td>
+            <td className="text-center">
+              {scoreData.simon ? scoreData.simon : "Not Played"}
+            </td>
+          </tr>
+          <tr>
+            <td>Dummy</td>
+            <td className="text-center">
+              {scoreData.dummy ? scoreData.dummy : "Not played"}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 export default GameHistory;
